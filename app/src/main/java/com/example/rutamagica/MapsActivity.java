ac99a6a95b3588394c20e2fff6265b0273b292f3
Reynaldo <reynaldo119@gmail.com>
2019-05-08 13:48:27 -0500

    Maps

--- app/src/main/java/com/example/rutamagica/MapsActivity.java
+++ app/src/main/java/com/example/rutamagica/MapsActivity.java
@@ -1,18 +1,31 @@
 package com.example.rutamagica;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private  MarkerOptions marcadorInicial;
+    private List<Address> addresses;
+    private Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +51,76 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-300, 51);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMaxZoomPreference(18);
+        mMap.setMinZoomPreference(11);
+
+        // Add a marker in tepozotlan and move the camera
+        LatLng tepozotlan = new LatLng(19.714060, -99.223510);
+
+        marcadorInicial = new MarkerOptions();
+        marcadorInicial.position(tepozotlan);
+        marcadorInicial.title("Marker Tepozotlan");
+        marcadorInicial.draggable(true);
+
+        mMap.addMarker(marcadorInicial);
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(tepozotlan)
+                .zoom(13)
+                .build();
+
+        CreadorIconos("Hacienda El campanario",19.714574,-99.218876);
+        CreadorIconos("Fuente del salto del agua",19.714391,-99.220058);
+
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+                double lat = marker.getPosition().latitude;
+                double lng = marker.getPosition().longitude;
+
+                try {
+                    addresses = geocoder.getFromLocation(lat,lng,1);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                String direccion = addresses.get(0).getAddressLine(0);
+
+                marker.setTitle(direccion);
+                marker.showInfoWindow();
+
+
+            }
+        });
+    }
+
+    public void CreadorIconos(String titulo,double lat, double lng){
+
+
+        LatLng posicion = new LatLng(lat,lng);
+
+        MarkerOptions marcador = new MarkerOptions();
+        marcador.position(posicion);
+        marcador.title(titulo);
+        marcador.draggable(false);
+        marcador.icon(BitmapDescriptorFactory.fromResource(android.R.drawable.star_on));
+
+        mMap.addMarker(marcador);
+
     }
 }
